5cab0b71d2de40b7e77276c67748a0c012641cc4
jmmauricio <jmmauricio7@gmail.com>
2018-11-13 20:38:29 +0100

    notebooks added

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'pydgrid'
+copyright = u'2018, Juan Manuel Mauricio'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'pydgriddoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -204,8 +204,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'pydgrid.tex', u'pydgrid Documentation',
+   u'pydgrid', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
