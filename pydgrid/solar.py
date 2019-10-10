6cf9a901c2c3cfa40bcc0dcfa212c9ab16feb279
pydgrid <jmmauricio6@gmail.com>
2019-10-10 21:23:38 +0200

    change path for file ClearnessIndexTPM

--- pydgrid/solar.py
+++ pydgrid/solar.py
@@ -40,11 +40,12 @@ class pv_gen(object):
 
         script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
         print(script_dir)
+        print('os.getcwd() = ' + os.getcwd())
          #df_ClearnessIndexTPM = pd.read_csv(os.path.join(os.path.dirname(__file__), "ClearnessIndexTPM.csv"))
         df_ClearnessIndexTPM = pd.read_csv(os.path.normcase(os.path.join(script_dir, 'ClearnessIndexTPM.csv')))
         self.df_ClearnessIndexTPM = df_ClearnessIndexTPM.set_index('Unnamed: 0')
         self.dKArray = np.array(self.df_ClearnessIndexTPM) 
-        print('os.getcwd() = ' + os.getcwd())
+        
 
 
     
