71d483ee4fcb9b6526abc37bbe7b12ac8166c02e
pydgrid <jmmauricio6@gmail.com>
2019-10-17 20:54:41 +0200

    cigre example to be run at raspberry pi

--- examples/cigre/cigre_europe_residential.py
+++ examples/cigre/cigre_europe_residential.py
@@ -6,6 +6,10 @@ Created on Fri Sep  8 09:15:54 2017
 @author: jmmauricio
 """
 
-import pydgrid
 
-sys1 = pydgrid.grid()
+import numpy as np
+from pydgrid.pydgrid import grid
+grid_1 = grid()
+grid_1.read('cigre_europe_residential.json')  # Load data
+grid_1.pf()
+print(grid_1.buses)
