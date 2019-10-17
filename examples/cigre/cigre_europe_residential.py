9471e4cb98ca0bd5815b9f4fc8734ccd9b2c1e5a
pydgrid <jmmauricio6@gmail.com>
2019-10-17 20:59:35 +0200

    cigre example to be run at raspberry pi

--- examples/cigre/cigre_europe_residential.py
+++ examples/cigre/cigre_europe_residential.py
@@ -9,7 +9,14 @@ Created on Fri Sep  8 09:15:54 2017
 
 import numpy as np
 from pydgrid.pydgrid import grid
+import time
+
 grid_1 = grid()
 grid_1.read('cigre_europe_residential.json')  # Load data
+
+time_0 = time.time()
 grid_1.pf()
+time_1 = time.time()
 print(grid_1.buses)
+
+print(f'Solution found in {time_1-time_0:0.4f} s')
